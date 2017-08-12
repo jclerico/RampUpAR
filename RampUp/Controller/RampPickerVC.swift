286870e710f76a76e5a843234f98d0c8ac0e6c0d
Jeremy Clerico <blops2hd@gmail.com>
2017-08-12 14:43:20 +0100

    3D models in SceneKit for popover

--- RampUp/Controller/RampPickerVC.swift
+++ RampUp/Controller/RampPickerVC.swift
@@ -30,6 +30,20 @@ class RampPickerVC: UIViewController {
         view.frame = CGRect(origin: CGPoint.zero, size: size)
         SceneView = SCNView(frame: CGRect(x: 0, y: 0, width: size.width, height: size.height))
         view.insertSubview(SceneView, at: 0)
+        
+        let scene = SCNScene(named: "art.scnassets/ramps.scn")!
+        SceneView.scene = scene
+        
+        let camera = SCNCamera()
+        camera.usesOrthographicProjection = true
+        scene.rootNode.camera = camera
+        
+        let obj = SCNScene(named: "art.scnassets/pipe.dae")
+        let node = obj?.rootNode.childNode(withName: "pipe", recursively: true)!
+        node?.scale = SCNVector3Make(0.0022, 0.0022, 0.0022)
+        node?.position = SCNVector3Make(-1, 0.7, -1)
+        scene.rootNode.addChildNode(node!)
+        
         preferredContentSize = size
     }
     
